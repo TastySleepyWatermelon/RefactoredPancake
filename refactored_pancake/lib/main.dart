be0e3de18d5ee36d38025f8b3111e2339c714613
MAlsh3lan <alshaalan911@gmail.com>
2026-09-14 17:45:34 +0300

    databais class

--- refactored_pancake/lib/main.dart
+++ refactored_pancake/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:refactored_pancake/screens/home_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    publishableKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const MainApp());
 }
 
