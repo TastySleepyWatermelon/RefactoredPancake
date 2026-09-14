2898024eb7b14d012b24083c3c4d2ef9c35383b0
MAlsh3lan <alshaalan911@gmail.com>
2026-09-14 12:32:57 +0300

    env file

--- refactored_pancake/lib/screens/meds_screen.dart
+++ refactored_pancake/lib/screens/meds_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyWidget extends StatelessWidget {
   const new({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(children: [
+        Text("Dad's medications",)
+
+
+      ],),
+
+
+    );
   }
 }
